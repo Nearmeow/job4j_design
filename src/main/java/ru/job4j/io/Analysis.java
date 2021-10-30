0aacf0259a8a1a7ada84aac2087897f5b5ab451f
Maria <ronios@yandex.ru>
2021-10-30 23:14:00 +0500

    2. Анализ доступности сервера. [#859 #222473]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -21,7 +21,7 @@ public class Analysis {
                     }
                 } else {
                     if (isStartTimeNoted) {
-                        out.print(String.format("%s;", lineArray[1]));
+                        out.println(String.format("%s;", lineArray[1]));
                         isStartTimeNoted = false;
                     }
                 }
@@ -31,13 +31,8 @@ public class Analysis {
         }
     }
 
-
     public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Analysis analysis = new Analysis();
+        analysis.unavailable("server.log", "target.log");
     }
 }
