54914c166b5350a193008ba00787653de7af5164
Maria <ronios@yandex.ru>
2021-11-08 15:20:42 +0500

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -1,12 +1,14 @@
 package ru.job4j.io;
 
 import java.io.File;
-import java.io.IOException;
 
 public class Dir {
 
-    public static void main(String[] args) throws IOException {
-        File file = new File("c:\\projects");
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,17 +1,22 @@
 package ru.job4j.io;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
 
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("C:/projects");
-        search(start, p -> p.toFile().getName().endsWith("js")).forEach(System.out::println);
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Not enough arguments. Usage java -jar dir.jar ROOT_FOLDER EXTENSION.");
+        }
+        Path start = Paths.get(args[0]);
+        String extension = args[1];
+        if (!Files.exists(start)) {
+            throw new NoSuchFileException("No such ROOT_FOLDER exist.");
+        }
+        search(start, p -> p.toFile().getName().endsWith(extension)).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
