24dcf43bfa4ee87933ac9ee2d8da31554dccc1f5
Maria <ronios@yandex.ru>
2021-11-09 02:27:56 +0500

    5. Валидация параметров запуска. [#246865 #226115]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,12 +8,12 @@ import java.util.function.Predicate;
 public class Search {
 
     public static void main(String[] args) throws IOException {
-        if (args.length < 2) {
-            throw new IllegalArgumentException("Not enough arguments. Usage java -jar dir.jar ROOT_FOLDER EXTENSION.");
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Wrong number of arguments. Usage java -jar dir.jar ROOT_FOLDER EXTENSION.");
         }
         Path start = Paths.get(args[0]);
         String extension = args[1];
-        if (!Files.exists(start)) {
+        if (!Files.isDirectory(start)) {
             throw new NoSuchFileException("No such ROOT_FOLDER exist.");
         }
         search(start, p -> p.toFile().getName().endsWith(extension)).forEach(System.out::println);
