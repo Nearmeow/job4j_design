fc6ff134937d2ad9f96eed70656ac614c6a374e0
Maria <ronios@yandex.ru>
2021-11-15 13:05:26 +0500

    5.2. Архивировать проект [#861 #228339]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -24,20 +24,8 @@ public class Zip {
         }
     }
 
-    public static void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         ArgsName argsName = ArgsName.of(args);
-
         String source = argsName.get("d");
         String exclude = argsName.get("e");
         String output = argsName.get("o");
@@ -50,6 +38,6 @@ public class Zip {
         }
         List<Path> listToPack = Search.search(sourcePath, path -> !path.toFile().getName().endsWith(exclude));
         List<File> files = listToPack.stream().map(Path::toFile).collect(Collectors.toList());
-        packFiles(files, new File(sourcePath.toFile().getParent() + "/" + output));
+        packFiles(files, new File(sourcePath.toFile() + "/" + output));
     }
 }
