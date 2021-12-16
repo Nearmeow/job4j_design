98d9583c49dbe7fe54e68640690df38ce73c2537
Maria <ronios@yandex.ru>
2021-12-17 02:47:35 +0500

    1. Inner join [#373617]

---
+++ sql/inner_join.sql
@@ -0,0 +1,26 @@
+create table address(
+    id serial primary key,
+    city varchar(255)
+);
+
+create table users(
+    id serial primary key,
+    name varchar(255),
+    address_id int references address(id)
+);
+
+insert into address(city) values ('Moscow');
+insert into address(city) values ('Perm');
+insert into address(city) values ('Sochi');
+
+insert into users(name, address_id) values ('Oleg', 1);
+insert into users(name, address_id) values ('Olga', 2);
+insert into users(name, address_id) values ('Peter', 1);
+insert into users(name, address_id) values ('Julia', 3);
+insert into users(name, address_id) values ('Maria', 2);
+insert into users(name, address_id) values ('Vlad', 1);
+
+select * from users join address on users.address_id = address.id;
+select * from users as u join address as a on u.address_id = a.id;
+select u.name as Имя, a.city as Город from users as u join address as a on u.address_id = a.id;
+select u.name as Имя, a.city as Город from users as u join address as a on u.address_id = a.id where a.city like '%o%';
